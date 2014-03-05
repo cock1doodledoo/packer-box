efab83147081b107b5563beba4d359f53e33419f
cock1doodledoo <cock1doodledoo@gmail.com>
2014-03-05 12:25:55 +0900

    fix console garble

--- scripts/locale.sh
+++ scripts/locale.sh
@@ -3,7 +3,9 @@
 # vagrant-user
 mkdir ~/.bashrc.d
 cat << '__EOF__' > ~/.bashrc.d/locale.sh
-export LANG=ja_JP.UTF-8
-export LANGUAGE=ja_JP:ja
+if [ ${TERM} != linux ]; then
+  export LANG=ja_JP.UTF-8
+  export LANGUAGE=ja_JP:ja
+fi
 __EOF__
 chown -R vagrant:vagrant ~/.bashrc.d
