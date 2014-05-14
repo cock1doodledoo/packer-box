9a18b81b37385cf76a59f6dd6895a54c024d121b
cock1doodledoo <cock1doodledoo@gmail.com>
2014-05-14 12:06:26 +0900

    update vagrant.pub url
    follow github website's raw-url

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -5,12 +5,11 @@ echo %vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
 
 # Installing vagrant keys
-if [ ! -d  /home/vagrant/.ssh ]
-then
+if [ ! -d  /home/vagrant/.ssh ]; then
   mkdir /home/vagrant/.ssh
   chmod 700 /home/vagrant/.ssh
 fi
 cd /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chmod 600 /home/vagrant/.ssh/authorized_keys
+wget 'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
