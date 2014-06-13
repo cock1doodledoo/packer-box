8a9bc41fb5697fc3a39506de51676e9e8fbd4a20
cock1doodledoo <cock1doodledoo@gmail.com>
2014-06-13 13:20:06 +0900

    delete logfile and *.box before build

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 set VM_NAME=base
 set LOG_FILE=build.log
 
+if exist %LOG_FILE% del %LOG_FILE%
+if exist %VM_NAME%.box del %VM_NAME%.box
 if exist output-virtualbox-iso\ rmdir /s /q output-virtualbox-iso
 if exist "%USERPROFILE%\VirtualBox VMs\%VM_NAME%" rmdir /s /q "%USERPROFILE%\VirtualBox VMs\%VM_NAME%"
 
